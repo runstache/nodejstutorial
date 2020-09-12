60b5f4ce10eaa5512a0151f3983aeb274e8ad939
Larry Shields <larry.shields@gmail.com>
2020-09-12 16:07:54 -0400

    Added CL for Timezone app

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World.");
+const moment = require('moment-timezone');
+moment.tz.setDefault('America/New_York');
+
+let targetTimezone;
+if (process.argv.length != 3) {
+  console.log('Ussage: node <script-file> <timezone>');
+  process.exit(1);
+} else {
+  targetTimezone = process.argv[2];
+  console.log(`The time at the ${targetTimezone} is ${moment().tz(targetTimezone).format()}`);
+}
